aa1d43c1bf48017cd666698c3358ed8a60e093d8
deecyn <deecyns@gmail.com>
2020-06-02 00:53:05 +0800

    init mall_leanr_15

---
+++ mall_learn_15/src/main/java/deecyn/mall_learn_15/MallLearn15Application.java
@@ -0,0 +1,13 @@
+package deecyn.mall_learn_15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallLearn15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallLearn15Application.class, args);
+	}
+
+}
---
+++ mall_learn_15/src/test/java/deecyn/mall_learn_15/MallLearn15ApplicationTests.java
@@ -0,0 +1,13 @@
+package deecyn.mall_learn_15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallLearn15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
